7dedbf2461dbb148b0765d8690c218d215fc8da9
Ewerton Igor <ewertonigor@icloud.com>
2024-03-04 23:34:46 -0300

    Criando variáveis de cores e Configurando pastas e componentes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import Head from 'next/head';
+import styles from '@/styles/HomeNoAuth.module.scss'
 
 const HomeNoAuth = () => {
   return (
     <>
       <Head>
-        <title>HOME</title>
+        <title>Onebitflix</title>
+        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+        <meta property="og:title" content="Onebitflix" key="title"/>
+        <meta name="description" content="Tenha acesso aos melhores conteúdos de programação de uma forma simples e fácil"/>
       </Head>
       <main></main>
     </>
