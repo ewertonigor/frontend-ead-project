c224d3333048ce108aa7f7e6a3456e44085f77e0
Ewerton Igor <ewertonigor@icloud.com>
2024-03-10 12:34:48 -0300

    Criação dos elemetnos da seção de favorites, do seu estilo visual e também dos métodos de favorites (Add and Delete) - Seção favoritos

---
+++ src/components/homeAuth/favoriteCategory/index.tsx
@@ -0,0 +1,26 @@
+import courseService from '@/src/services/courseService'
+import styles from '../../../../styles/slideCategory.module.scss'
+import useSWR from 'swr'
+import SlideComponent from '../../common/slideComponent'
+
+const FavoriteCategory = () => {
+  const { data, error } = useSWR('/favorites', courseService.getFavCourse)
+
+  if (error) return error
+  if (!data) return (<><p>Loading...</p></>)
+
+  return (
+    <>
+      <p className={styles.titleCategory}>Minha lista</p>
+      {data.data.courses.length >= 1 ? (
+        <SlideComponent course={data.data.courses}/>
+      ) : (
+        <p className='text-center pt-3 h5'>
+          <strong>Você não tem nenhum curso na lista</strong>
+        </p>
+      )}
+    </>
+  )
+}
+
+export default FavoriteCategory
--- src/services/courseService.ts
+++ src/services/courseService.ts
@@ -20,8 +20,6 @@ export type CourseType = {
 const courseService = {
     getNewestCourses: async () => {
         const res = await api.get('/courses/newest').catch((err) => {
-            console.log(err.response.data.message)
-
             return err.response
         })
 
@@ -36,8 +34,46 @@ const courseService = {
         }
       })
       .catch((err) => {
-        console.log(err.resonse.data.message)
+        return err.response
+      })
 
+      return res
+    },
+    addToFav: async (courseId: number | string) => {
+      const token = sessionStorage.getItem('onebitflix-token')
+
+      const res = await api.post('/favorites', {courseId}, {
+        headers: {
+          Authorization: `Bearer $${token}`
+        }
+      }).catch(err => {
+        return err.response
+      })
+
+      return res
+    },
+    removeFav: async (courseId: number | string) => {
+      const token = sessionStorage.getItem('onebitflix-token')
+
+      const res = await api.delete('/favorites', {
+        headers: {
+          Authorization: `Bearer ${token}`
+        },
+        data: { courseId }
+      }).catch(err => {
+        return err.response
+      })
+
+      return res
+    },
+    getFavCourse: async () => {
+      const token = sessionStorage.getItem('onebitflix-token')
+
+      const res = await api.get('/favorites', {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }).catch(err => {
         return err.response
       })
 
