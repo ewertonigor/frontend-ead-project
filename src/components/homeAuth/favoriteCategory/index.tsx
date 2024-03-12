3d0f7924536c555c32f8244381d8bf493e10e7d1
Ewerton Igor <ewertonigor@icloud.com>
2024-03-12 15:28:36 -0300

    Criação do pageSpiner, da sua estrutura e do seu estilo visual - Page Spinner

--- src/components/homeAuth/favoriteCategory/index.tsx
+++ src/components/homeAuth/favoriteCategory/index.tsx
@@ -2,12 +2,13 @@ import courseService from '@/src/services/courseService'
 import styles from '../../../../styles/slideCategory.module.scss'
 import useSWR from 'swr'
 import SlideComponent from '../../common/slideComponent'
+import PageSpinner from '../../common/spinner'
 
 const FavoriteCategory = () => {
   const { data, error } = useSWR('/favorites', courseService.getFavCourse)
 
   if (error) return error
-  if (!data) return (<><p>Loading...</p></>)
+  if (!data) return (<PageSpinner />)
 
   return (
     <>
--- src/components/homeAuth/newestCategory/index.tsx
+++ src/components/homeAuth/newestCategory/index.tsx
@@ -2,12 +2,13 @@ import styles from '../../../../styles/slideCategory.module.scss'
 import courseService from "@/src/services/courseService"
 import useSWR from "swr"
 import SlideComponent from "../../common/slideComponent"
+import PageSpinner from '../../common/spinner'
 
 const NewestCategory = function () {
   const { data, error } = useSWR('/newest', courseService.getNewestCourses)
 
   if (error) return error
-  if (!data) return (<><p>Loading...</p></>)
+  if (!data) return (<PageSpinner />)
 
   return (
     <>
